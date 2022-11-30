12dc3149518c4b1c254e28fd80574ed6baa16f71
rajivjajo <rajivjajodia82552@gmail.com>
2022-11-30 16:29:23 +0530

    Add Part time Employee & Wage - Assume Part time Hour is 8

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,23 @@
 isPresent=$((RANDOM%3));
+partimeEmpHr=8;
+wagePerHr=20;
+Emp=0;
 if [ $isPresent -eq 0 ]
 then
 	echo "Employee is absent";
+	workingHr=0;
+	Emp="Emp is absent";
 	
 elif [ $isPresent -eq 1 ]
 then
 	echo "Employee is present";
+	workingHr=16;
+	Emp="Emp is full-time"
 	
 else
 	echo "Employee is working as part time";
-	
+		workingHr=$partimeEmpHr;
+		Emp="Emp is part-time"
 fi
+wages=$(($wagePerHr * $workingHr));
+echo "$Emp and has earned $wages";
