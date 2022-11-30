441fb2e490ce86d9af853b8f641fbcbc511c3b22
rajivjajo <rajivjajodia82552@gmail.com>
2022-11-30 17:08:02 +0530

    Monthly wages of employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,27 @@
+#Calculating Wages for a Month
+#Assume 20 Working Day per Month
+
 isPresent=$((RANDOM%3));
-if [ $isPresent -eq 0 ]
+wagePerHr=20;
+monthlyWorkingDays=20;
+if [ $isPresent -eq 1 ]
 then
-	echo "Employee is absent";
+	echo "Employee is Present";
+	workingHr=8;
 	
-elif [ $isPresent -eq 1 ]
+	
+elif [ $isPresent -eq 2 ]
 then
-	echo "Employee is present";
+	echo "Employee is part-time present";
+	workingHr=4;
+	
 	
 else
-	echo "Employee is working as part time";
-	
+	echo "Employee is absent";
+		workingHr=0;
 fi
+		
+wages=$(($wagePerHr * $workingHr));
+monthlyWage=$(($wages * $monthlyWorkingDays));
+
+echo "$monthlyWage is Monthly wage for the employee";
