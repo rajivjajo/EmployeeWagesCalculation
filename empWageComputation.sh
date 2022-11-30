1d3bc3193218722085d1c67a15188af8a657af70
rajivjajo <rajivjajodia82552@gmail.com>
2022-11-30 16:06:31 +0530

    Daily wages calculation for Employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,19 @@
 isPresent=$((RANDOM%3));
+workingHour=8
+wagesPerHour=20
 if [ $isPresent -eq 0 ]
 then
 	echo "Employee is absent";
+		workingHour=0;
 	
 elif [ $isPresent -eq 1 ]
 then
 	echo "Employee is present";
-	
+		workingHour=8;
 else
 	echo "Employee is working as part time";
+		workingHour=$((workingHour/2));
 	
 fi
+Wage=$(($workingHour * $wagesPerHour));
+echo "Employee has earned $Wage today";
